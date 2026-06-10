30e454b5abd67b20e15e36f93b4af6cf6191b97c
Daniel Vieites <dvieitest@gmail.com>
2026-06-10 16:53:01 +0200

    chore: remove sketchybar

--- sketchybar/plugins/cpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-usage=$(ps -A -o %cpu | awk '{s+=$1} END {print int(s / 8) "%"}')
-
-sketchybar --set "$NAME" label="$usage"
--- sketchybar/plugins/memory.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-usage=$(ps -A -o %mem | awk '{s+=$1} END {print int(s) "%"}')
-
-sketchybar --set "$NAME" label="$usage"
